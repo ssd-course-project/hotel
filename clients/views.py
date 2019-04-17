613ec1086ba948922f8402100c3a6c180dfe3556
vladimirbokor <35324009+vladimirbokor@users.noreply.github.com>
2019-04-17 14:33:25 +0200

    t14-add-client-profile (#19)

--- clients/views.py
+++ clients/views.py
@@ -1,13 +1,30 @@
+from django.core.exceptions import ValidationError
 from django.views import generic
 
-from .forms import RegistrationForm
+from clients.forms import RegistrationForm
+from clients.models import Client
 
 
 class RegisterView(generic.FormView):
     template_name = 'registration/register.html'
     form_class = RegistrationForm
-    success_url = '/'
+    success_url = '/accounts/login'
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+class ProfileView(generic.TemplateView):
+    template_name = 'clients/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        if user and not any((user.is_superuser, user.is_staff)):
+            try:
+                context['client'] = Client.objects.get(user=user)
+            except Client.DoesNotExist:
+                raise ValidationError("You are not our client!")
+        return context
