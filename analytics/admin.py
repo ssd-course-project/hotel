dff7f149eb6a7fb66104a94e59c64b53e06d4af3
vladimirbokor <vladimirbokor@mail.ru>
2019-04-19 17:24:39 +0200

    t12-booking-analytics

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 
-from analytics.models import Feedback
+from analytics.models import Feedback, RoomBooking
 
 
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('author', 'created_at', 'rating', 'text')
 
 
+class RoomBookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'room',
+        'renter',
+        'check_in_date',
+        'check_out_date',
+        'created_at'
+    )
+    readonly_fields = (
+        'room',
+        'renter',
+        'check_in_date',
+        'check_out_date',
+        'created_at'
+    )
+
+
 admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(RoomBooking, RoomBookingAdmin)
+
--- hotel/urls.py
+++ hotel/urls.py
@@ -4,11 +4,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.RoomList.as_view(), name='room_list'),
-    path('room/<int:pk>/', views.RoomDetail.as_view(), name='room_detail'),
+    path('', views.RoomListView.as_view(), name='room_list'),
+    path('room/<int:pk>/', views.RoomDetailView.as_view(), name='room_detail'),
     path(
         'room/<int:pk>/booking',
-        login_required(views.RoomBooking.as_view()),
+        login_required(views.RoomBookingView.as_view()),
         name='room_booking'
     ),
 ]
