bb0708a2b102f8fcb22fd10b815f0fd29b4c4fe5
Cellofun <freckled.cellofun@gmail.com>
2019-04-21 00:53:22 +0700

    ghost pics fix

--- hotel/static/vendors/jquery/menu/menu.js
+++ hotel/static/vendors/jquery/menu/menu.js
@@ -2,7 +2,7 @@ $(function() {
 
     $('ul.menu > li').hover(
         function(){
-            $(this).find('ul.menu__list').fadeIn(200);
+            $(this).find('ul.menu__list').fadeIn(200).css({'position': 'absolute', 'z-index':'1000'});
 
         }, function() {
             $(this).find('ul.menu__list').fadeOut(200);
