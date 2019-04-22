edbf8a8ddcf04bc74dc49b6343d4d326c4034f7d
vladimirbokor <35324009+vladimirbokor@users.noreply.github.com>
2019-04-22 17:30:20 +0200

    t36-test-booking-form (#37)

--- hotel/tests.py
+++ hotel/tests.py
@@ -1,3 +1,67 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from freezegun import freeze_time
+
+from hotel.forms import RoomBookingForm
+
+
+class RoomBookingFormTest(TestCase):
+
+    def test_form_is_valid(self):
+        check_in_date = datetime.date(2019, 4, 23)
+        check_out_date = datetime.date(2019, 4, 25)
+
+        form = RoomBookingForm(
+            data={
+                "check_in_date": check_in_date,
+                "check_out_date": check_out_date
+            }
+        )
+        with freeze_time("2019-04-21"):
+            self.assertTrue(form.is_valid())
+
+    def test_check_in_date_before_current(self):
+        error_message = "Выберите дату бронирования позднее чем сегодня"
+        check_in_date = datetime.date(2019, 4, 19)
+        check_out_date = datetime.date(2019, 4, 25)
+
+        form = RoomBookingForm(
+            data={
+                "check_in_date": check_in_date,
+                "check_out_date": check_out_date
+            }
+        )
+        with freeze_time("2019-04-21"):
+            self.assertFalse(form.is_valid())
+            assert error_message == form.errors['__all__'][0]
+
+    def test_check_out_date_before_current(self):
+        error_message = "Выберите дату бронирования позднее чем сегодня"
+        check_in_date = datetime.date(2019, 4, 22)
+        check_out_date = datetime.date(2019, 4, 20)
+
+        form = RoomBookingForm(
+            data={
+                "check_in_date": check_in_date,
+                "check_out_date": check_out_date
+            }
+        )
+        with freeze_time("2019-04-21"):
+            self.assertFalse(form.is_valid())
+            assert error_message == form.errors['__all__'][0]
+
+    def test_check_in_date_after_check_out_date(self):
+        error_message = "Дата отъезда не может быть раньше даты заезда"
+        check_in_date = datetime.date(2019, 4, 25)
+        check_out_date = datetime.date(2019, 4, 22)
+
+        form = RoomBookingForm(
+            data={
+                "check_in_date": check_in_date,
+                "check_out_date": check_out_date
+            }
+        )
+        with freeze_time("2019-04-21"):
+            self.assertFalse(form.is_valid())
+            assert error_message == form.errors['__all__'][0]
