4d9b0c02cc2fcb49afbea694c827ee1596058eec
Cellofun <freckled.cellofun@gmail.com>
2019-04-24 23:45:43 +0700

    search view test trial

--- hotel/tests.py
+++ hotel/tests.py
@@ -1,9 +1,11 @@
 import datetime
 
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 from freezegun import freeze_time
 
 from hotel.forms import RoomBookingForm
+from hotel.models import Room
 
 
 class RoomBookingFormTest(TestCase):
@@ -69,31 +71,88 @@ class RoomBookingFormTest(TestCase):
 
 class RoomSearchViewTest(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('room_search')
+        self.room1 = Room.objects.create(
+            title='testRoom1',
+            price=20000,
+            sleeps_number=2,
+            main_picture='../hotel/static/vendors/images/general/background.png'
+        )
+        self.room2 = Room.objects.create(
+            title='testRoom2',
+            price=10000,
+            sleeps_number=3,
+            main_picture='../hotel/static/vendors/images/general/background.png'
+        )
+        self.booking1 = RoomBookingForm(
+            data={
+                "room": self.room1,
+                "check_in_date": datetime.date(2019, 4, 21),
+                "check_out_date": datetime.date(2019, 4, 25)
+            }
+        )
+        self.booking1 = RoomBookingForm(
+            data={
+                "room": self.room2,
+                "check_in_date": datetime.date(2019, 5, 21),
+                "check_out_date": datetime.date(2019, 5, 25)
+            }
+        )
+
     def test_general_get_request(self):
-        response = self.client.get("/rooms/")
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "hotel/room_search.html")
+        self.assertQuerysetEqual(
+            response.context['rooms'],
+            ['<Room: Номер 20м2 на 2 человек>', '<Room: Номер 20м2 на 3 человек>'],
+            ordered=False
+        )
 
     def test_get_request_without_date(self):
-        response = self.client.get("/rooms/", {"check_in_date": "2019-04-21"})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "hotel/room_search.html")
-        self.assertFalse(False, "Введите дату заезда и выезда")
+        error_message = "" \
+                        "Введите дату заезда и выезда, чтобы мы могли показать вам " \
+                        "доступные в это время номера"
+        response = self.client.get(
+            self.url,
+            {"check_in_date": "2019-04-21"}
+        )
+        self.assertEqual(error_message, response.context_data.get('error_message'))
+        # self.assertQuerysetEqual(response.context['rooms'], [])
 
     def test_get_request_with_dates(self):
-        response = self.client.get("/rooms/", {"check_in_date": "2019-04-21", "check_out_date": "2019-04-25"})
+        response = self.client.get(
+            self.url, {
+                "check_in_date": "2019-04-21",
+                "check_out_date": "2019-04-25"
+            }
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "hotel/room_search.html")
-        self.assertTrue(True, response.content)
+        # self.assertQuerysetEqual(
+        #     response.context['rooms'],
+        #     ['<Room: Номер 20м2 на 2 человек>']
+        # )
 
     def test_get_request_with_visitors(self):
-        response = self.client.get('/rooms/', {"visitors": "2"})
+        response = self.client.get(
+            self.url,
+            {"visitors": "2"}
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'hotel/room_search.html')
-        self.assertTrue(True, response.content)
+        self.assertQuerysetEqual(
+            response.context['rooms'],
+            ['<Room: Номер 20м2 на 2 человек>']
+        )
 
     def test_get_request_with_price(self):
-        response = self.client.get('/rooms/', {"price": "10000"})
+        response = self.client.get(
+            self.url,
+            {"price": "10000"}
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'hotel/room_search.html')
-        self.assertTrue(True, response.content)
+        self.assertQuerysetEqual(
+            response.context['rooms'],
+            ['<Room: Номер 20м2 на 3 человек>']
+        )
