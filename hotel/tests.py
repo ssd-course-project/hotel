bf6b9824f4b0ea3be07bb006b9c84eb467af86bd
Olessya Khussainova <freckled.cellofun@gmail.com>
2019-04-26 20:31:24 +0700

    T39 test room search view (#41)
    * t39-test-room-search-view
    
    * search view test trial
    
    * one more test trial
    
    * fixed by review
    
    * removed unused import

--- hotel/tests.py
+++ hotel/tests.py
@@ -1,9 +1,14 @@
 import datetime
 
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 from freezegun import freeze_time
 
+from analytics.models import RoomBooking
 from hotel.forms import RoomBookingForm
+from hotel.models import Room
+from clients.models import Client
 
 
 class RoomBookingFormTest(TestCase):
@@ -65,3 +70,103 @@ class RoomBookingFormTest(TestCase):
         with freeze_time("2019-04-21"):
             self.assertFalse(form.is_valid())
             assert error_message == form.errors['__all__'][0]
+
+
+class RoomSearchViewTest(TestCase):
+
+    def setUp(self):
+        self.url = reverse('room_search')
+        self.user = User.objects.create(
+            username='testuser',
+            email='test@test.com',
+            password='qwerty123',
+        )
+        self.renter = Client.objects.create(
+            user=self.user,
+            name='test',
+            phone=88005553535,
+            email='test@test.com'
+        )
+        self.room1 = Room.objects.create(
+            title='testRoom1',
+            price=20000,
+            sleeps_number=2,
+            main_picture='../hotel/static/vendors/images/general/background.png'
+        )
+        self.room2 = Room.objects.create(
+            title='testRoom2',
+            price=10000,
+            sleeps_number=3,
+            main_picture='../hotel/static/vendors/images/general/background.png'
+        )
+        self.booking = RoomBooking.objects.create(
+            room=self.room1,
+            renter=self.renter,
+            check_in_date=datetime.date(2019, 4, 21),
+            check_out_date=datetime.date(2019, 4, 25)
+        )
+
+    def test_general_get_request(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "hotel/room_search.html")
+        self.assertQuerysetEqual(
+            response.context.get('object_list'),
+            map(repr, [self.room1, self.room2]),
+            ordered=False
+        )
+
+    def test_get_request_without_date(self):
+        error_message = "" \
+                        "Введите дату заезда и выезда, чтобы мы могли " \
+                        "показать Вам доступные в это время номера"
+        with freeze_time("2019-04-21"):
+            response = self.client.get(
+                self.url,
+                {"check_in": "22.04.2019"}
+            )
+        self.assertEqual(
+            error_message,
+            response.context_data.get('error_message')
+        )
+        self.assertQuerysetEqual(
+            response.context.get('object_list'),
+            map(repr, [self.room1, self.room2]),
+            ordered=False
+        )
+
+    def test_get_request_with_dates(self):
+        with freeze_time("2019-04-21"):
+            response = self.client.get(
+                self.url, {
+                    "check_in": "22.04.2019",
+                    "check_out": "24.04.2019"
+                }
+            )
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context.get('object_list'),
+            map(repr, [self.room2])
+        )
+
+    def test_get_request_with_visitors(self):
+        response = self.client.get(
+            self.url,
+            {"visitors": "2"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context.get('object_list'),
+            map(repr, [self.room1])
+        )
+
+    def test_get_request_with_price(self):
+        response = self.client.get(
+            self.url,
+            {"price": "10000"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context.get('object_list'),
+            map(repr, [self.room2])
+        )
